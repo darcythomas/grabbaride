6deb84e1ff1863d06a80d51eb374ef05dd67e8ab
gen.battle <gen.battle@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-07-20 11:00:17 +0000

    Split the frontend into login and main pages; was going to add a search page but thought that is prettymuch the main page anyway. Started adding AJAX code.

---
+++ grabbaride/trunk/GrabbaRide.Frontend/AJAX/grabbaride.js
@@ -0,0 +1,31 @@
+﻿//This file is in the GrabbaRide Namespace
+if (!GrabbaRide) { GrabbaRide = new Object(); }
+
+
+/**
+* AJAXRequest - fires a standard POST message to the page specified by url, sending it content.
+* When the response is recieved, the data is passed to function specified by callback.
+* TODO: Needs type checking, make sure callback is a function, check whether we want 
+* to pass the responsetext or xml data.
+* Constructed from code on the page http://en.wikipedia.org/wiki/XMLHttpRequest
+**/
+GrabbaRide.AJAXRequest = function(url, content, callback)
+{
+    if( typeof XMLHttpRequest == "undefined" )
+        alert("Your browser doesn't support the scripting neccesary to run this website properly");
+    var request =  new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-Type",
+                           "application/x-javascript;");
+ 
+    if (callback) {
+        request.onreadystatechange = function() {
+            if (request.readyState == 4 && request.status == 200) {
+                if (request.responseText) {
+                    callback(request.responseText);
+                }
+            }
+        }  
+    }
+    request.send(content);
+}
