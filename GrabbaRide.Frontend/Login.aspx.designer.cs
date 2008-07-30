9d8d889b5b80d5521dd650361d212655eea2847a
Nick Sarten <gen.battle@gmail.com>
2008-07-30 10:01:24 +0000

    Added authentication to User.aspx so that only users who are logged in can view the page. A non logged-in user will be redirected to the login page. Modified the login page so if a user tries to visit it when they are already logged in it will redirect them to Default.aspx.

--- GrabbaRide.Frontend/Login.aspx.designer.cs
+++ GrabbaRide.Frontend/Login.aspx.designer.cs
@@ -21,5 +21,14 @@ namespace GrabbaRide.Frontend {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Login Login1;
+        
+        /// <summary>
+        /// reglink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink reglink;
     }
 }
