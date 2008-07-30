ff3a1c486cbcdb9e00596b17e80ce53f1c756026
darcy@darcythomas.com <darcy@darcythomas.com@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-07-30 10:52:48 +0000

    Created some clean snazzy (much better than waht I was trying to do before) methods to get a a ride out of the DB and to put one in.

--- grabbaride/trunk/GrabbaRide.ObjectMappings/CallDB.cs
+++ grabbaride/trunk/GrabbaRide.ObjectMappings/CallDB.cs
@@ -8,57 +8,33 @@ namespace GrabbaRide.ObjectMappings
     class CallDB
     {
 
-        /*
-         * SubmitTrip()
-         * Takes the int ID for the the city to and from locations
-         * The date as a datetime.
-         * The int userID
-         * 
-         * This then creates a new instance of a ride
-         * 
-         **/
-       public void SubmitTrip(int nameFrom, int nameTo, DateTime date, int userID)
-       {
-
-           DataClassesDataContext context = new DataClassesDataContext();
-           Ride ride = new Ride();
-               
-           ride.FromLocation = nameFrom;
-           ride.ToLocation = nameTo;
-           ride.Date = date;
-           ride.UserID = userID;
-
-           context.Rides.InsertOnSubmit(ride);
-                context.SubmitChanges();
-       }
-
-
-
-
-        public void GetATrip(int rideID)
+        public Object getRideDetails(int rideID)
         {
-
-            DataClassesDataContext context = new DataClassesDataContext();
-           
+            DataClasses1DataContext context = new DataClasses1DataContext();
 
             var q = from Ride r in context.Rides
                     where r.RideID == rideID
-                    select new { userID = r.UserID, fromLoc = r.FromLocation, toLoc = r.ToLocation, date = r.Date };
+                    select new Ride { };
 
             if (q.Count() > 0)
             {
-                 //Do something with the data, pass it back to someone or some thing.
+                return q;
             }
             else
             {
-                //Crap I dont know; you work it out.
-                //i.e., no such rideID
+                return null;
+                //RideID not valid.
             }
-
-
         }
 
+        public void AddRide(Ride newRide)
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
 
-        
+            context.Rides.InsertOnSubmit(newRide);
+            context.SubmitChanges();
+        }
+
     }
 }
+
