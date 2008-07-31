9d82bcb18721897139c3667d08d9d7901a00f6a3
Darcy <darcythomas@users.noreply.github.com>
2008-07-31 01:57:07 +0000

    Fixed: (IDENTITY_INSERT) bug in DBCreateScript. Re: issue 15
    Added: two change scripts. One to add some users to the DB
    (It happens to be us, FYI all your birthdates are the date the internet was last reset)
    
    The other script will add various locations to the DB.

---
+++ GrabbaRide.Database/Change Scripts/AddLocations.sql
@@ -0,0 +1,20 @@
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('Vegas', 36.196633, -115.136719)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('Monte Carlo', 43.7398, 7.4272)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('Atlantis', 360.0, -360.0)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('Mt Doom, Mordor',-39.304700, 175.526240)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('South Pole', -180.0, 175.617230)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('New Washington', 33.299313, 44.395752)
+
+INSERT INTO Location (LocationID, Name, long, lat)
+VALUES ('Massey', -40.385765, 175.617779)
