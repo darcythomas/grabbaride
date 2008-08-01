651a43dbc4ce57acf6853328f30a424a1144570d
a.palamountain <a.palamountain@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-08-01 23:51:56 +0000

    Application Testing Here

---
+++ grabbaride/trunk/ApplicationTests/LinqDatabaseCreationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GrabbaRide.Database;
+
+
+namespace ApplicationTests
+{
+    [TestFixture]
+    public class LinqDatabaseCreationTests
+    {
+        [Test] 
+        public void CreationTest()
+        {
+            GrabbaRideDBDataContext context = new GrabbaRideDBDataContext();
+            context.CreateDatabase();
+            Assert.IsTrue(context.DatabaseExists());
+          
+        }
+
+    }
+}
