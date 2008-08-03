627ab2d90a744bf1cce96bf582f7bd4bad9d558f
adrian.macneil <adrian.macneil@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-08-03 10:23:19 +0000

    Renamed test project to 'GrabbaRide.UnitTests' and changed the project type to 'Test Project' so that visual studio can be configured to automatically run our test cases after a build (when the time comes), and so that new test cases can be automatically generated and integrated with visual studio. Kept Amy's database creation test case.

--- grabbaride/trunk/GrabbaRide.UnitTests/Properties/AssemblyInfo.cs
+++ grabbaride/trunk/GrabbaRide.UnitTests/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ApplicationTests")]
+[assembly: AssemblyTitle("GrabbaRide.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApplicationTests")]
+[assembly: AssemblyProduct("GrabbaRide.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3ff34906-d5fb-464a-9d2a-2a52dc19afd9")]
+[assembly: Guid("15adda79-5e5d-47ed-ab14-63773bbb87ad")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
