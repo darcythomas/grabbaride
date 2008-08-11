d9106b415f1a35c0a6fc674969e2f80280fe29cf
a.palamountain <a.palamountain@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-08-11 13:11:39 +0000

    Bare bones (extreamly bare) of class libs for system maintainance and Membership providers SLEEP

---
+++ grabbaride/trunk/SystemManagement/GrabbaRideMembershipProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace SystemManagement
+{
+    public sealed class GrabbaRideMembershipProvider //: MembershipProvider
+    {
+
+        #region Class Variables
+
+        private int newPasswordLength = 8;
+        private string connectionString;
+        private string applicationName;
+        private bool enablePasswordReset;
+        private bool enablePasswordRetrieval;
+        private bool requiresQuestionAndAnswer;
+        private bool requiresUniqueEmail;
+        private int maxInvalidPasswordAttempts;
+        private int passwordAttemptWindow;
+        private MembershipPasswordFormat passwordFormat;
+        private int minRequiredNonAlphanumericCharacters;
+        private int minRequiredPasswordLength;
+        private string passwordStrengthRegularExpression;
+
+
+        #endregion
+
+        #region Enums
+
+        private enum FailureType
+        {
+            Password = 1,
+            PasswordAnswer = 2
+        }
+
+        #endregion
+
+        #region Properties
+
+       
+
+        #endregion
+
+        #region Initialization
+
+        
+        #endregion
+
+    }
+}
