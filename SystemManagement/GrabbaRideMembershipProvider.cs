2c4c080f9538b8d13f727310fd20a702028c490b
Amy Palamountain <amy@github.com>
2008-08-11 21:50:04 +0000

    Sorted out or web.config intalisation of Membership provider

--- SystemManagement/GrabbaRideMembershipProvider.cs
+++ SystemManagement/GrabbaRideMembershipProvider.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Security;
+using System.Collections.Specialized;
+using System.Web.Configuration;
+using System.Configuration;
+using System.Configuration.Provider;
 
 namespace SystemManagement
 {
-    public sealed class GrabbaRideMembershipProvider //: MembershipProvider
+    public sealed class GrabbaRideMembershipProvider : MembershipProvider
     {
 
         #region Class Variables
 
-        private int newPasswordLength = 8;
-        private string connectionString;
-        private string applicationName;
-        private bool enablePasswordReset;
-        private bool enablePasswordRetrieval;
-        private bool requiresQuestionAndAnswer;
-        private bool requiresUniqueEmail;
-        private int maxInvalidPasswordAttempts;
-        private int passwordAttemptWindow;
-        private MembershipPasswordFormat passwordFormat;
-        private int minRequiredNonAlphanumericCharacters;
-        private int minRequiredPasswordLength;
-        private string passwordStrengthRegularExpression;
+        private int _newPasswordLength;
+        private string _connectionString;
+        private string _applicationName;
+        private bool _enablePasswordReset;
+        private bool _enablePasswordRetrieval;
+        private bool _requiresQuestionAndAnswer;
+        private bool _requiresUniqueEmail;
+        private int _maxInvalidPasswordAttempts;
+        private int _passwordAttemptWindow;
+        private MembershipPasswordFormat _passwordFormat;
+        private int _minRequiredNonAlphanumericCharacters;
+        private int _minRequiredPasswordLength;
+        private string _passwordStrengthRegularExpression;
+        private MachineKeySection _machineKey; //Used when determining encryption key values.
 
 
         #endregion
@@ -46,8 +51,238 @@ namespace SystemManagement
 
         #region Initialization
 
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException("config");
+            }
+
+            if (name == null || name.Length == 0)
+            {
+                name = "GrabbaRideMembershipProvider";
+            }
+
         
+            //Initialize the abstract base class.
+            base.Initialize(name, config);
+
+            _applicationName = GetConfigValue(config["ApplicationName"], System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath);
+            // These should be set in web.config but this class also contains defult values also just incase!!
+            _maxInvalidPasswordAttempts = Convert.ToInt32(GetConfigValue(config["maxInvalidPasswordAttempts"], "5"));
+            //?? Shrug.... lets let the base class deal with this for now 10 seconds is a good window of opp nice and safe
+            _passwordAttemptWindow = Convert.ToInt32(GetConfigValue(config["passwordAttemptWindow"], "10"));
+           
+            _minRequiredNonAlphanumericCharacters = Convert.ToInt32(GetConfigValue(config["minRequiredAlphaNumericCharacters"], "1"));
+            _minRequiredPasswordLength = Convert.ToInt32(GetConfigValue(config["minRequiredPasswordLength"], "7"));
+        
+            // Amy your up to here
+            _passwordStrengthRegularExpression = Convert.ToString(GetConfigValue(config["passwordStrengthRegularExpression"], String.Empty));
+            _enablePasswordReset = Convert.ToBoolean(GetConfigValue(config["enablePasswordReset"], "true"));
+            _enablePasswordRetrieval = Convert.ToBoolean(GetConfigValue(config["enablePasswordRetrieval"], "true"));
+            _requiresQuestionAndAnswer = Convert.ToBoolean(GetConfigValue(config["requiresQuestionAndAnswer"], "false"));
+            _requiresUniqueEmail = Convert.ToBoolean(GetConfigValue(config["requiresUniqueEmail"], "true"));
+
+            string temp_format = config["passwordFormat"];
+            if (temp_format == null)
+            {
+                temp_format = "Hashed";
+            }
+
+            switch (temp_format)
+            {
+                case "Hashed":
+                    _passwordFormat = MembershipPasswordFormat.Hashed;
+                    break;
+                case "Encrypted":
+                    _passwordFormat = MembershipPasswordFormat.Encrypted;
+                    break;
+                case "Clear":
+                    _passwordFormat = MembershipPasswordFormat.Clear;
+                    break;
+                default:
+                    throw new ProviderException("Password format not supported.");
+            }
+
+            ConnectionStringSettings _ConnectionStringSettings = ConfigurationManager.ConnectionStrings[config["connectionStringName"]];
+
+            if ((_ConnectionStringSettings == null) || (_ConnectionStringSettings.ConnectionString.Trim() == String.Empty))
+            {
+                throw new ProviderException("Connection string cannot be blank.");
+            }
+
+            _connectionString = _ConnectionStringSettings.ConnectionString;
+
+            //Get encryption and decryption key information from the configuration.
+            System.Configuration.Configuration cfg = WebConfigurationManager.OpenWebConfiguration(System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath);
+            _machineKey = cfg.GetSection("system.web/machineKey") as MachineKeySection;
+
+            if (_machineKey.ValidationKey.Contains("AutoGenerate"))
+            {
+                if (PasswordFormat != MembershipPasswordFormat.Clear)
+                {
+                    throw new ProviderException("Hashed or Encrypted passwords are not supported with auto-generated keys.");
+                }
+            }
+        }
+
+        private string GetConfigValue(string configValue, string defaultValue)
+        {
+            if (String.IsNullOrEmpty(configValue))
+            {
+                return defaultValue;
+            }
+
+            return configValue;
+        }
         #endregion
+
+        #region Implementation of MembershipProvider Methods
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+
 
     }
 }
