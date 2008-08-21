be08da9bcef70e733c7c17e9a2a029bffa000677
Adrian Macneil <adrianmacneil@users.noreply.github.com>
2008-08-21 04:17:55 +0000

    Fixed the database project and LINQ mappings to reflect the new schema.

--- GrabbaRide.Database/Ride.cs
+++ GrabbaRide.Database/Ride.cs
@@ -5,5 +5,28 @@ namespace GrabbaRide.Database
     public partial class Ride
     {
         public int SearchRank { get; set; }
+
+        public TimeSpan DepartureTimeSpan
+        {
+            get { return new TimeSpan(this.DepartureTime); }
+            set { this.DepartureTime = value.Ticks; }
+        }
+
+        public TimeSpan JourneyTimeSpan
+        {
+            get { return new TimeSpan(this.JourneyLength); }
+            set { this.JourneyLength = value.Ticks; }
+        }
+
+        public Ride(double locFromLat, double locFromLong, double locToLat, double locToLong)
+            : this()
+        {
+            this.Available = true;
+            this.CreationDate = DateTime.Now;
+            this.LocationFromLat = locFromLat;
+            this.LocationFromLong = locFromLong;
+            this.LocationToLat = locToLat;
+            this.LocationToLong = locToLong;
+        }
     }
 }
--- GrabbaRide.Database/User.cs
+++ GrabbaRide.Database/User.cs
@@ -1,46 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data.Linq;
-using System.Data.Linq.Mapping;
-using System.Data;
-using System.Reflection;
-using System.Linq.Expressions;
-using System.ComponentModel;
-using System;
-
+﻿using System.Linq;
 
 namespace GrabbaRide.Database
 {
-    public partial class User 
+    public partial class User
     {
-        public User(string firstName, string lastName, Gender gender, DateTime DOB, string username,
-            string password, string email)
+        public User(string username)
+            : this()
         {
-            this._Rides = new EntitySet<Ride>(new Action<Ride>(this.attach_Rides), new Action<Ride>(this.detach_Rides));
-            this._OpenIDs = new EntitySet<OpenID>(new Action<OpenID>(this.attach_OpenIDs), new Action<OpenID>(this.detach_OpenIDs));
-            OnCreated();
-            this.FirstName = firstName;
-            this.LastName = lastName;
-            this.Gender = gender;
-            this.DOB = DOB;
             this.Username = username;
-            this.Password = password;
-            this.Email = email;
-            OnCreated();
         }
 
-
-        public User getUser(string userName)
+        public static User GetUser(string username)
         {
             GrabbaRideDBDataContext context = new GrabbaRideDBDataContext();
             var user = from u in context.Users
-                       where u._Username == userName
+                       where u.Username == username
                        select u;
             return (User)user;
-            
         }
-
-        
     }
 }
