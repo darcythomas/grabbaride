7c8cba262882ca0d040ec7fa0ab0220ad90a986f
adrian.macneil <adrian.macneil@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-08 10:22:18 +0000

    Don't know what the fuck was happening with that file, but it compiles now :)

--- grabbaride/trunk/GrabbaRide.Database/GrabbaRideDB.cs
+++ grabbaride/trunk/GrabbaRide.Database/GrabbaRideDB.cs
@@ -153,17 +153,11 @@ namespace GrabbaRide.Database
         /// <param name="username">The username of the user to search for.</param>
         /// <returns>A User object, or null if no user was found.</returns>
         public User GetUserByUsername(string username)
-
         {
             User result = (User)from u in Users
                                 where u.Username == username
                                 select u;
             return result;
-
-
-
-
-
         }
 
 
@@ -187,220 +181,40 @@ namespace GrabbaRide.Database
             return query.Skip(pageIndex * pageSize).Take(pageSize);
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-namespace GrabbaRide.Database
-{
-
-    partial class GrabbaRideDBDataContext
-    {
-
-        #region RideMappings
-
-        /// <summary>
-        /// Gets the ride owned by the id
-        /// </summary>
-        /// <param name="rideID"></param>
-        /// <returns></returns>
-        public Ride getRide(int rideID)
-        {
-
-            var ride = from r in this.Rides
-                       where r.RideID == rideID
-                       select r;
-            return (Ride)ride;
-        }
-
-        /// <summary>
-        /// Returns All rides after the specifed date
-        /// </summary>
-        /// <param name="afterDate"></param>
-        /// <returns></returns>
-        public List<Ride> getRidesAfterDate(DateTime afterDate)
-        {
-
-            var rides = from r in this.Rides
-                        where r.StartDate >= afterDate
-                        select r;
-            return (List<Ride>)rides;
-        }
-
-
-        /// <summary>
-        /// Returns all rides before the specifed date
-        /// </summary>
-        /// <param name="beforeDate"></param>
-        /// <returns></returns>
-        public List<Ride> getRidesBeforeDate(DateTime beforeDate)
-        {
-
-            var rides = from r in this.Rides
-                        where r.EndDate <= beforeDate
-                        select r;
-            return (List<Ride>)rides;
-        }
-
-
-        /// <summary>
-        /// Returns all rides between the specifed Dates
-        /// </summary>
-        /// <param name="beforeDate"></param>
-        /// <param name="afterDate"></param>
-        /// <returns></returns>
-        public List<Ride> getRidesBetween(DateTime beforeDate, DateTime afterDate)
-        {
-            var rides = from r in this.getRidesBeforeDate(beforeDate)
-                        where r.StartDate >= afterDate
-                        select r;
-            return (List<Ride>)rides;
-
-        }
-
-
-        /// <summary>
-        /// Returns all rides avalible
-        /// </summary>
-        /// <returns></returns>
-        public List<Ride> getAllAvalibleRides()
-        {
-            var rides = from r in this.Rides
-                        where r.NumSeats > 0 && r.Available
-                        select r;
-            return (List<Ride>)rides;
-        }
-
-        /// <summary>
-        /// Returns all rides belonging to userID
-        /// </summary>
-        /// <param name="userID"></param>
-        /// <returns></returns>
-        public List<Ride> getUserRides(int userID)
-        {
-            var rides = from r in this.Rides
-                        where r.UserID == userID
-                        select r;
-            return (List<Ride>)rides;
-        }
-
-
-        /// <summary>
-        /// Returns all rides that reocur for a given day
-        /// Day is an enum dayofweek
-        /// </summary>
-        /// <param name="day"></param>
-        /// <returns></returns>
-        public List<Ride> getAllRidesRecur(DayOfWeek day)
-        {
-            List<Ride> rides = null;
-
-            switch (day)
-            {
-                case DayOfWeek.Monday: rides = (List<Ride>)from r in this.Rides
-                                                           where r.RecurMon
-                                                           select r;
-                    break;
-                case DayOfWeek.Tuesday: rides = (List<Ride>)from r in this.Rides
-                                                            where r.RecurTue
-                                                            select r;
-                    break;
-                case DayOfWeek.Wednesday: rides = (List<Ride>)from r in this.Rides
-                                                              where r.RecurWed
-                                                              select r;
-                    break;
-                case DayOfWeek.Thursday: rides = (List<Ride>)from r in this.Rides
-                                                             where r.RecurThu
-                                                             select r;
-                    break;
-                case DayOfWeek.Friday: rides = (List<Ride>)from r in this.Rides
-                                                           where r.RecurMon
-                                                           select r;
-                    break;
-                case DayOfWeek.Saturday: rides = (List<Ride>)from r in this.Rides
-                                                             where r.RecurSat
-                                                             select r;
-                    break;
-                case DayOfWeek.Sunday: rides = (List<Ride>)from r in this.Rides
-                                                           where r.RecurSun
-                                                           select r;
-                    break;
-            }
-
-            return rides;
-        }
-
-        #endregion
-
-        #region UserMappings
-
-        public User GetUser(string username)
-        {
-
-            var user = from u in this.Users
-                       where u.Username == username
-                       select u;
-            return (User)user;
-        }
-
         public List<User> GetAllUsers()
         {
-            List<User> user = List < User > from u in this.Users
-                                            select u;
-
-            return user;
+            return (List<User>)from u in this.Users
+                               select u;
         }
 
 
 
         public List<User> GetUser_SortByUserName()
         {
-            List<User> user = List < User > from u in this.Users
-                                            orderby u.Username
-                                            select u;
-
-            return user;
+            return (List<User>)from u in this.Users
+                               orderby u.Username
+                               select u;
         }
 
         public List<User> GetUser_SortByIsOnline()
         {
-            List<User> user = List < User > from u in this.Users
-                                            orderby u.IsOnline
-                                            select u;
+            return (List<User>)from u in this.Users
+                               orderby u.IsOnline
+                               select u;
         }
 
         public List<User> GetUser_SortByOccupation()
         {
-            List<User> user = List < User > from u in this.Users
-                                            orderby u.Occupation
-                                            select u;
+            return (List<User>)from u in this.Users
+                               orderby u.Occupation
+                               select u;
         }
 
         public List<User> GetUser_SortByLastActvityDate()
         {
-            List<User> user = List < User > from u in this.Users
-                                            orderby u.LastActvityDate
-                                            select u;
+            return (List<User>)from u in this.Users
+                               orderby u.LastActvityDate
+                               select u;
         }
 
 
