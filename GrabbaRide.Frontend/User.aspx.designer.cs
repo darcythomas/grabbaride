28a8bd407fcac83bbe1d38170a2bed27732ab857
Adrian Macneil <adrianmacneil@users.noreply.github.com>
2008-09-08 23:05:21 +0000

    User profile page works. Also the membership provider now sets the last password changed date when you change the password.

--- GrabbaRide.Frontend/User.aspx.designer.cs
+++ GrabbaRide.Frontend/User.aspx.designer.cs
@@ -14,12 +14,21 @@ namespace GrabbaRide.Frontend {
     public partial class WebForm2 {
         
         /// <summary>
-        /// Label1 control.
+        /// UserDataSource control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.ObjectDataSource UserDataSource;
+        
+        /// <summary>
+        /// UserDetailsView control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView UserDetailsView;
     }
 }
