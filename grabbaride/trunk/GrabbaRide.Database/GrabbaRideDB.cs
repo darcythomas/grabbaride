1c5ce80710209d089658773bb2bc3b9fdb145a70
adrian.macneil <adrian.macneil@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-08 10:17:24 +0000

    Find users by their username

--- grabbaride/trunk/GrabbaRide.Database/GrabbaRideDB.cs
+++ grabbaride/trunk/GrabbaRide.Database/GrabbaRideDB.cs
@@ -98,6 +98,212 @@ namespace GrabbaRide.Database
             return (List<Ride>)rides;
         }
 
+        /// <summary>
+        /// Returns all rides that reocur for a given day
+        /// Day is an enum dayofweek
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public List<Ride> getAllRidesRecur(DayOfWeek day)
+        {
+            List<Ride> rides = null;
+
+            switch (day)
+            {
+                case DayOfWeek.Monday: rides = (List<Ride>)from r in this.Rides
+                                                           where r.RecurMon
+                                                           select r;
+                    break;
+                case DayOfWeek.Tuesday: rides = (List<Ride>)from r in this.Rides
+                                                            where r.RecurTue
+                                                            select r;
+                    break;
+                case DayOfWeek.Wednesday: rides = (List<Ride>)from r in this.Rides
+                                                              where r.RecurWed
+                                                              select r;
+                    break;
+                case DayOfWeek.Thursday: rides = (List<Ride>)from r in this.Rides
+                                                             where r.RecurThu
+                                                             select r;
+                    break;
+                case DayOfWeek.Friday: rides = (List<Ride>)from r in this.Rides
+                                                           where r.RecurMon
+                                                           select r;
+                    break;
+                case DayOfWeek.Saturday: rides = (List<Ride>)from r in this.Rides
+                                                             where r.RecurSat
+                                                             select r;
+                    break;
+                case DayOfWeek.Sunday: rides = (List<Ride>)from r in this.Rides
+                                                           where r.RecurSun
+                                                           select r;
+                    break;
+            }
+
+            return rides;
+        }
+
+        #endregion
+
+        #region UserMappings
+
+        /// <summary>
+        /// Finds a user by their username.
+        /// </summary>
+        /// <param name="username">The username of the user to search for.</param>
+        /// <returns>A User object, or null if no user was found.</returns>
+        public User GetUserByUsername(string username)
+
+        {
+            User result = (User)from u in Users
+                                where u.Username == username
+                                select u;
+            return result;
+
+
+
+
+
+        }
+
+
+        /// <summary>
+        /// Finds users whose usernames contain usernameToMatch. The results are limited by pageIndex and pageSize.
+        /// </summary>
+        /// <param name="usernameToMatch">A username to search for.</param>
+        /// <param name="pageIndex">The offset page index (starts from 0).</param>
+        /// <param name="pageSize">The page size.</param>
+        /// <param name="totalRecords">Returns the total number of records </param>
+        /// <returns>A list of Users that match.</returns>
+        public IEnumerable<User> FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            var query = from u in Users
+                        where u.Username.Contains(usernameToMatch)
+                        orderby u.Username
+                        select u;
+
+            totalRecords = query.Count();
+
+            return query.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace GrabbaRide.Database
+{
+
+    partial class GrabbaRideDBDataContext
+    {
+
+        #region RideMappings
+
+        /// <summary>
+        /// Gets the ride owned by the id
+        /// </summary>
+        /// <param name="rideID"></param>
+        /// <returns></returns>
+        public Ride getRide(int rideID)
+        {
+
+            var ride = from r in this.Rides
+                       where r.RideID == rideID
+                       select r;
+            return (Ride)ride;
+        }
+
+        /// <summary>
+        /// Returns All rides after the specifed date
+        /// </summary>
+        /// <param name="afterDate"></param>
+        /// <returns></returns>
+        public List<Ride> getRidesAfterDate(DateTime afterDate)
+        {
+
+            var rides = from r in this.Rides
+                        where r.StartDate >= afterDate
+                        select r;
+            return (List<Ride>)rides;
+        }
+
+
+        /// <summary>
+        /// Returns all rides before the specifed date
+        /// </summary>
+        /// <param name="beforeDate"></param>
+        /// <returns></returns>
+        public List<Ride> getRidesBeforeDate(DateTime beforeDate)
+        {
+
+            var rides = from r in this.Rides
+                        where r.EndDate <= beforeDate
+                        select r;
+            return (List<Ride>)rides;
+        }
+
+
+        /// <summary>
+        /// Returns all rides between the specifed Dates
+        /// </summary>
+        /// <param name="beforeDate"></param>
+        /// <param name="afterDate"></param>
+        /// <returns></returns>
+        public List<Ride> getRidesBetween(DateTime beforeDate, DateTime afterDate)
+        {
+            var rides = from r in this.getRidesBeforeDate(beforeDate)
+                        where r.StartDate >= afterDate
+                        select r;
+            return (List<Ride>)rides;
+
+        }
+
+
+        /// <summary>
+        /// Returns all rides avalible
+        /// </summary>
+        /// <returns></returns>
+        public List<Ride> getAllAvalibleRides()
+        {
+            var rides = from r in this.Rides
+                        where r.NumSeats > 0 && r.Available
+                        select r;
+            return (List<Ride>)rides;
+        }
+
+        /// <summary>
+        /// Returns all rides belonging to userID
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        public List<Ride> getUserRides(int userID)
+        {
+            var rides = from r in this.Rides
+                        where r.UserID == userID
+                        select r;
+            return (List<Ride>)rides;
+        }
+
 
         /// <summary>
         /// Returns all rides that reocur for a given day
