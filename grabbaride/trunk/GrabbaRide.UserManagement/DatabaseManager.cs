672a0380a95733f1d7449a0b9e7dd2db16a1e437
adrian.macneil <adrian.macneil@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-08 13:48:38 +0000

    Renamed UserManagement project to fit naming scheme.

---
+++ grabbaride/trunk/GrabbaRide.UserManagement/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemManagement
+{
+    /// <summary>
+    /// preforms the required maintainance on the database such as
+    ///     enures that users arnt entered more than once
+    ///     Ride maintaince
+    ///     Recurrence of ride
+    ///     Ride entry and exit
+    ///     ensures that no conflict arises in any feilds
+    ///     
+    ///     
+    /// </summary>
+    class DatabaseManager
+    {
+    }
+
+   
+
+
+}
---
+++ grabbaride/trunk/GrabbaRide.UserManagement/GrabbaRideMembershipProvider.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using System.Collections.Specialized;
+using System.Web.Configuration;
+using System.Configuration;
+using System.Configuration.Provider;
+using SystemManagement.Properties;
+using GrabbaRide.Database;
+
+namespace SystemManagement
+{
+    public sealed class GrabbaRideMembershipProvider : MembershipProvider
+    {
+
+        #region Class Variables
+
+        GrabbaRideDBDataContext _dataContext;
+
+        #endregion
+
+        #region Enums
+
+        private enum FailureType
+        {
+            Password = 1,
+            PasswordAnswer = 2
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Initialization
+
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException("config cannot be null");
+            }
+
+            if (String.IsNullOrEmpty(name))
+            {
+                name = "GrabbaRideMembershipProvider";
+            }
+
+            //Initialize the abstract base class.
+            base.Initialize(name, config);
+
+            _dataContext = new GrabbaRideDBDataContext();
+
+            /*
+            string temp_format = config["passwordFormat"];
+            if (temp_format == null)
+            {
+                temp_format = "Hashed";
+            }
+
+            switch (temp_format)
+            {
+                case "Hashed":
+                    _passwordFormat = MembershipPasswordFormat.Hashed;
+                    break;
+                case "Encrypted":
+                    _passwordFormat = MembershipPasswordFormat.Encrypted;
+                    break;
+                case "Clear":
+                    _passwordFormat = MembershipPasswordFormat.Clear;
+                    break;
+                default:
+                    throw new ProviderException("Password format not supported.");
+             *
+            }
+             
+
+            ConnectionStringSettings _ConnectionStringSettings = ConfigurationManager.ConnectionStrings[config["connectionStringName"]];
+
+            if ((_ConnectionStringSettings == null) || (_ConnectionStringSettings.ConnectionString.Trim() == String.Empty))
+            {
+                throw new ProviderException("Connection string cannot be blank.");
+            }
+
+            _connectionString = _ConnectionStringSettings.ConnectionString;
+
+            //Get encryption and decryption key information from the configuration.
+            System.Configuration.Configuration cfg = WebConfigurationManager.OpenWebConfiguration(System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath);
+            _machineKey = cfg.GetSection("system.web/machineKey") as MachineKeySection;
+
+            if (_machineKey.ValidationKey.Contains("AutoGenerate"))
+            {
+                if (PasswordFormat != MembershipPasswordFormat.Clear)
+                {
+                    throw new ProviderException("Hashed or Encrypted passwords are not supported with auto-generated keys.");
+                }
+            }
+            */
+        }
+        #endregion
+
+        #region Implementation of MembershipProvider Methods
+
+        public override string ApplicationName { get; set; }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
