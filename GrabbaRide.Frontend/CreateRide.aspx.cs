90bec8c95a4ac4eeae99d766be092a8a6313c298
Nick Sarten <gen.battle@gmail.com>
2008-09-08 01:56:19 +0000

    CreateRide.aspx will now create a basic ride. Still missing some fields when added to the DB, but i didn't really know what to do with them. This at least gives us a basic add ride framework to work within. Also the userid is currently permanently set to "1", while we wait for the authentication module to be completed.

--- GrabbaRide.Frontend/CreateRide.aspx.cs
+++ GrabbaRide.Frontend/CreateRide.aspx.cs
@@ -27,9 +27,35 @@ namespace GrabbaRide.Frontend
             }
         }
 
-        protected void DepartureTimeCalendar_SelectionChanged(object sender, EventArgs e)
-        {   
-            
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            String fromloc = hfstart.Value;
+            String toloc = hfend.Value;
+            String[] loctemp;
+            System.Collections.Specialized.ListDictionary listDictionary = new System.Collections.Specialized.ListDictionary();
+            listDictionary.Add("CreationDate", DateTime.Now);
+            listDictionary.Add("StartDate", calstart.SelectedDate);
+            listDictionary.Add("EndDate", calEnd.SelectedDate);
+            listDictionary.Add("NumSeats", drpSeats.SelectedValue);
+            listDictionary.Add("RecurMon", chkmon.Checked);
+            listDictionary.Add("RecurTue", chktue.Checked);
+            listDictionary.Add("RecurWed", chkwed.Checked);
+            listDictionary.Add("RecurThu", chkthurs.Checked);
+            listDictionary.Add("RecurFri", chkfri.Checked);
+            listDictionary.Add("RecurSat", chksat.Checked);
+            listDictionary.Add("RecurSun", chksun.Checked);
+            loctemp = fromloc.Split(',');
+            listDictionary.Add("LocationFromLat", loctemp[0]);
+            listDictionary.Add("LocationFromLong", loctemp[1]);
+            loctemp = toloc.Split(',');
+            listDictionary.Add("LocationToLat", loctemp[0]);
+            listDictionary.Add("LocationToLong", loctemp[1]);
+            listDictionary.Add("UserID", 1);
+            listDictionary.Add("Available", true);
+
+            CreateRideSource.Select = "";
+            CreateRideSource.Insert(listDictionary);
+
         }
     }
 }
