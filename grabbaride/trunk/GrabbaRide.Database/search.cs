d489ca50f98175a5c7c798d4a5a31d1653b264d7
darcy@darcythomas.com <darcy@darcythomas.com@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-09 05:12:10 +0000

    The search class should no longer be broken. Passing the search.getRides() method a ride object will return a list of ranked similar Ride objects
    Methods in the search class have been commented.

--- grabbaride/trunk/GrabbaRide.Database/search.cs
+++ grabbaride/trunk/GrabbaRide.Database/search.cs
@@ -7,12 +7,17 @@ namespace GrabbaRide.Database
 {
     class search
     {
-
+        /// <summary>
+        /// Finds Rides in the DB which are similar to the submitted param ride object
+        /// </summary>
+        /// <param name="submittedRide"></param>
+        /// <returns>A list of Ride objects found in the DB which are similar to the submittedRide param. 
+        /// The list is ranked based on similarty to the param submittedRide</returns>
       List<Ride>  getRides(Ride submittedRide)
         {
             List<Ride> rankedRides = new List<Ride>();
           float locToll = 0.01f;
-          int timeTol = 1800;
+          var timeTol = 1800;
             GrabbaRideDBDataContext db = new GrabbaRideDBDataContext();
 
           var q = from p in db.Rides
@@ -20,63 +25,44 @@ namespace GrabbaRide.Database
                   && p.LocationFromLat  <= (submittedRide.LocationFromLat + locToll) 
                   && p.LocationFromLong >= (submittedRide.LocationFromLong - locToll)
                   && p.LocationFromLong <= (submittedRide.LocationFromLong + locToll)
-                  && p.DepartureTime >= (submittedRide.DepartureTime - timeTol)
-                  && p.DepartureTime <= (submittedRide.DepartureTime + timeTol)
+                  && p.DepartureTime.TotalMinutes >= (submittedRide.DepartureTime.TotalMinutes - timeTol)
+                  && p.DepartureTime.TotalMinutes <= (submittedRide.DepartureTime.TotalMinutes + timeTol)
                select p;
              
+          //This takes one of the ride returned by the LINQ db query,
+          // runs the ranking algorithm (updating the SearchRank field) on it and then puts it into 
+          // an array list
           foreach(var p in db.Rides)
           {
               this.RideRank(p, submittedRide);
              rankedRides.Add(p);
           }
-          rankedRides.Sort();
-         // rankedRides.Sort(Ride r1, Ride r2) 
-                 //  { return r1.SearchRank - r2.SearchRank ; });
+
+          //This sorts the List according to the values in the SearchRank field of the Ride object
+          rankedRides.Sort(delegate(Ride r1, Ride r2)
+          {
+              int returnValue = -1;
+              if (r1.SearchRank > r2.SearchRank)
+              {
+                  returnValue = 1;
+              }
+              
+              return returnValue;
+          });
+
+          // Return the ranked list of possibly suitable rides
           return rankedRides;
         }
     
-     // public interface ride IComparable rideCompare{
-     //     if ( 
-      // }
+   
 
-          public class ride : IComparable<ride>
-{
-              /**
-       public int CompareTo(object obj)
-    {
-        if (obj is ride)
-        {
-            ride p2 = (ride)obj;
-            return _firstname.CompareTo(p2.Firstname);
-        }
-        else
-            throw new ArgumentException("Object is not a Person.");
-    }
-    */
-
-    #region IComparable<ride> Members
-
-    int IComparable<ride>.CompareTo(ride other)
-    {
-        if (obj is Ride)
-        {
-            Ride r = (Ride)obj;
-            return r.SearchRank;
-        }
-        else
-            throw new ArgumentException("Object is not a ride object.");
-    }
-
-       
-
-    #endregion
-}
-
-
-
-
-
-        Ride RideRank(Ride rr, Ride submittedRide)
+        /// <summary>
+        /// Sets the SearchRank value of one Ride object, based on the simalarty to another Ride object 
+        /// </summary>
+        /// <param name="rideToRank"></param>
+        /// <param name="submittedRide"></param>
+        /// <returns>A ride object the same as the rideToRank pram but with the SearchRank value modified</returns>
+        Ride RideRank(Ride rideToRank, Ride submittedRide)
         {
            double rank=0;
 
@@ -84,15 +70,15 @@ namespace GrabbaRide.Database
          float timeScale = 1.0f;
 
 
-         rank += Math.Pow(1 + Math.Abs(rr.LocationFromLat - submittedRide.LocationFromLat), 2) * distanceScale;
+         rank += Math.Pow(1 + Math.Abs(rideToRank.LocationFromLat - submittedRide.LocationFromLat), 2) * distanceScale;
 
-         rank += Math.Pow(1 + Math.Abs(rr.LocationFromLong - submittedRide.LocationFromLong), 2) * distanceScale;
+         rank += Math.Pow(1 + Math.Abs(rideToRank.LocationFromLong - submittedRide.LocationFromLong), 2) * distanceScale;
 
-         rank += Math.Pow(1 + Math.Abs(rr.DepartureTime - submittedRide.DepartureTime), 2) * timeScale;
+         rank += Math.Pow((rideToRank.DepartureTime - submittedRide.DepartureTime).TotalMinutes, 2) * timeScale;
                 
-           rr.SearchRank = rank;
+           rideToRank.SearchRank = rank;
 
-           return rr;
+           return rideToRank;
 
         }
     }
