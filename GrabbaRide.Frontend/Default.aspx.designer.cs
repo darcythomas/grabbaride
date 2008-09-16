789eee8c943ddfd8569ea49ce384918a934c4da3
Adrian Macneil <adrianmacneil@users.noreply.github.com>
2008-09-16 10:32:12 +0000

    Added sweet sweet b basics homepage.

--- GrabbaRide.Frontend/Default.aspx.designer.cs
+++ GrabbaRide.Frontend/Default.aspx.designer.cs
@@ -12,14 +12,5 @@ namespace GrabbaRide.Frontend {
     
     
     public partial class Default {
-        
-        /// <summary>
-        /// btnSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSearch;
     }
 }
