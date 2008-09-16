29c4429e0cacf8590c586560423f84d2d7373836
a.palamountain <a.palamountain@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-16 11:15:07 +0000

    OpenID libs added

---
+++ grabbaride/trunk/GrabbaRide.Frontend/OpenIDLogin.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using ExtremeSwank.OpenID;
+using ExtremeSwank.OpenID.PlugIns.Extensions;
+
+
+namespace GrabbaRide.Frontend
+{
+    public partial class OpenIDLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                OpenIDConsumer openid = GetConsumer();
+
+                // Read the arguments in the current request and
+                // automatically validate any OpenID responses,
+                // firing events when actions occur.
+                openid.HandleResponses();
+
+            }
+        }
+
+        private OpenIDConsumer GetConsumer()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
