821f1cd1f932bbd94dec9a7863a165c341eeeb07
michelle.cho.81 <michelle.cho.81@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-22 10:23:38 +0000

    remove 'button' in frontend 'create ride'

--- grabbaride/trunk/GrabbaRide.Frontend/CreateRide.aspx.designer.cs
+++ grabbaride/trunk/GrabbaRide.Frontend/CreateRide.aspx.designer.cs
@@ -149,15 +149,6 @@ namespace GrabbaRide.Frontend {
         protected global::System.Web.UI.WebControls.CheckBox chksun;
         
         /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
         /// btnCreate control.
         /// </summary>
         /// <remarks>
