b150fe985405e41d89016ba1800efa583f6f5df0
Adrian Macneil <adrianmacneil@users.noreply.github.com>
2008-09-30 02:08:53 +0000

    Added basic send email functionality to our library

--- GrabbaRide.Database/User.cs
+++ GrabbaRide.Database/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Web.Security;
+using System.Net.Mail;
+using System.Net;
 
 namespace GrabbaRide.Database
 {
@@ -9,6 +11,14 @@ namespace GrabbaRide.Database
             : this()
         {
             this.Username = username;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return String.Format("{0} {1}", FirstName, LastName);
+            }
         }
 
         /// <summary>
@@ -44,6 +54,33 @@ namespace GrabbaRide.Database
                 // otherwise, must be logged in
                 else return true;
             }
+        }
+
+        /// <summary>
+        /// Emails a message to the user, using a template plus the message.
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        public void SendMessage(string message, User fromUser)
+        {
+            const string MESSAGE_TEMPLATE = "You have recieved a new message from GrabbaRide user {0}:\r\n" +
+                "{1}\r\nThank you for using GrabbaRide!\r\nIf you have any queries, feel free to contact us " +
+                "at grabbaride@gmail.com\r\n";
+
+            // get the from and to addresses
+            MailAddress grabbarideAddress = new MailAddress("grabbaride@gmail.com", "GrabbaRide Team");
+            MailAddress userAddress = new MailAddress(this.Email, this.FullName);
+
+            // create the email
+            MailMessage email = new MailMessage(grabbarideAddress, userAddress);
+            email.ReplyTo = new MailAddress(fromUser.Email, fromUser.FullName);
+            email.Subject = "New message from GrabbaRide user";
+            email.Body = String.Format(MESSAGE_TEMPLATE, fromUser.Username, message);
+
+            // send the message
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("grabbaride@gmail.com", "letmeinplease");
+            client.Send(email);
         }
     }
 }
