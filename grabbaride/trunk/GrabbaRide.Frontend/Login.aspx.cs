7d0191cdc1941786d25fcecd63952755e1ae4508
a.palamountain <a.palamountain@119253b4-2b52-0410-b16a-1f6cb3b86101>
2008-09-30 06:32:26 +0000

    general

--- grabbaride/trunk/GrabbaRide.Frontend/Login.aspx.cs
+++ grabbaride/trunk/GrabbaRide.Frontend/Login.aspx.cs
@@ -10,6 +10,8 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Xml.Linq;
+using DotNetOpenId.RelyingParty;
+using DotNetOpenId.Extensions.SimpleRegistration;
 
 namespace GrabbaRide.Frontend
 {
@@ -17,24 +19,54 @@ namespace GrabbaRide.Frontend
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string redirectUrl = Request.QueryString["RedirectUrl"];
-            if (String.IsNullOrEmpty(redirectUrl))
+            if (Page.IsPostBack)
             {
-                redirectUrl = "Default.aspx";
-            }
 
-            if (Request.IsAuthenticated)
-            {
-                Response.Redirect(redirectUrl);
-            }
-            else
-            {
-                GrabbaRideLogin.DestinationPageUrl = redirectUrl;
-                GrabbaRideLogin.CreateUserUrl += "?RedirectUrl=" + redirectUrl;
+                //determine who done the post back
+             
+              // OpenIdLogin1.LoggingIn()
+                string redirectUrl = Request.QueryString["RedirectUrl"];
+
+                if (String.IsNullOrEmpty(redirectUrl))
+                {
+                    redirectUrl = "Default.aspx";
+                }
+
+                if (Request.IsAuthenticated)
+                {
+                    Response.Redirect(redirectUrl);
+                }
+                else
+                {
+                    GrabbaRideLogin.DestinationPageUrl = redirectUrl;
+                    GrabbaRideLogin.CreateUserUrl += "?RedirectUrl=" + redirectUrl;
+                }
             }
         }
 
-     
+
+        /// <summary>
+        /// Fired upon login.
+        /// Note, that straight after login, forms auth will redirect the user to their original page. So this page may never be rendererd.
+        /// </summary>
+        protected void OpenIdLogin1_LoggedIn(object sender, OpenIdEventArgs e)
+        {
+            testbox.Text = "Logged in " + e.Response.FriendlyIdentifierForDisplay;
+        }
+        protected void OpenIdLogin1_Failed(object sender, OpenIdEventArgs e)
+        {
+           
+        }
+        protected void OpenIdLogin1_Canceled(object sender, OpenIdEventArgs e)
+        {
+           
+        }
+        protected void OpenIdLogin1_SetupRequired(object sender, OpenIdEventArgs e)
+        {
+           
+        }
+
+       
 
      
     }
