70405ca31c40d633b1d32ece4fa1971bf4ab7733
Adrian Macneil <adrianmacneil@users.noreply.github.com>
2008-10-13 23:09:50 +0000

    add create a ride link in search page when no results are found

--- GrabbaRide.Frontend/Search.aspx.designer.cs
+++ GrabbaRide.Frontend/Search.aspx.designer.cs
@@ -32,6 +32,15 @@ namespace GrabbaRide.Frontend {
         protected global::System.Web.UI.WebControls.Label SearchResultsLabel;
         
         /// <summary>
+        /// CreateRideLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl CreateRideLink;
+        
+        /// <summary>
         /// ResultsGridView control.
         /// </summary>
         /// <remarks>
